bf75ea991bb928e0787f103e3d367d1639df30d4
ArtemCherchuk <250artem250@gmail.com>
2023-11-26 12:23:21 +0100

    add createSelector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,32 +2,20 @@ import React, { useEffect } from 'react';
 import css from 'components/ContactList/ContactList.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact, fetchContacts } from 'redux/contacts/contacts.reducer';
-import {
-  selectContacts,
-  selectFilter,
-} from 'redux/contacts/contacts.selectors';
+import { selectVisibleContacts } from 'redux/contacts/contacts.selectors';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
+  const contacts = useSelector(selectVisibleContacts);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  const filterContacts = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
-
-  const filteredContacts = filterContacts();
-
   return (
     <ul className={css.list}>
-      {filteredContacts.length > 0 ? (
-        filteredContacts.map(({ id, name, phone }) => {
+      {contacts.length > 0 ? (
+        contacts.map(({ id, name, phone }) => {
           return (
             <li key={id} className={css.listItem}>
               <p>{name}</p>
